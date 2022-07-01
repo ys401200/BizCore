466ebd6b414800fdb0bc1eb6b9b2df0cbc6c1600
jo-beoungil <enky001@gmail.com>
2022-07-01 17:17:48 +0900

    로그인함수 오류 재수정

--- src/main/java/kr/co/bizcore/v1/svc/UserService.java
+++ src/main/java/kr/co/bizcore/v1/svc/UserService.java
@@ -8,7 +8,7 @@ import kr.co.bizcore.v1.domain.User;
 public class UserService extends Svc {
 
     public User getBasicUserInfo(String userNo) {
-        User result = userMapper.getBasicUserInfo2(userNo);
+        User result = userMapper.getBasicUserInfo(userNo);
         return result;
     } // End of getBasicUserInfo
 
