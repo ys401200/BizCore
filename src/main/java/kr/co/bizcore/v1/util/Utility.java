ee5a8b41c59f5bc74c57924e73b764cd5b2fa3a6
jo-beoungil <enky001@gmail.com>
2022-07-01 14:38:32 +0900

    암호화 작업중

--- src/main/java/kr/co/bizcore/v1/util/Utility.java
+++ src/main/java/kr/co/bizcore/v1/util/Utility.java
@@ -39,6 +39,41 @@ public class Utility {
     // 암호화
     // ===============================================================================
 
+    private String generateKey() {
+        return generateKey(32);
+    } // End of generateKey()
+
+    private String generateKey(int length) {
+        byte[] data = null, src = new byte[69];
+        int x = 0;
+        int t = 0;
+
+        for (x = 0; x < src.length; x++) {
+            if (x < 10)
+                src[x] = (byte) (x + 48);
+            else if (x < 36)
+                src[x] = (byte) (x + 55);
+            else if (x < 62)
+                src[x] = (byte) (x + 61);
+            else if (x == 63)
+                src[x] = (byte) 33;
+            else if (x == 64)
+                src[x] = (byte) 43;
+            else if (x == 65)
+                src[x] = (byte) 126;
+            else
+                src[x] = (byte) (x - 31);
+        }
+
+        data = new byte[length];
+        for (x = 0; x < length; x++) {
+            t = (int) (Math.random() * src.length);
+            data[x] = src[t];
+        }
+
+        return new String(data);
+    } // End of generateKey()
+
     // AES 암호화 메서드
     public String encAes(String message) {
         return message;
