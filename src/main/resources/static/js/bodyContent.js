e7fd1b71cb6d8919a645bd9d3f2ddc36f0a6656c
5079daeng <myy32612@gmail.com>
2022-07-13 11:07:19 +0900

    공지사항 함수 추가

--- src/main/resources/static/js/bodyContent.js
+++ src/main/resources/static/js/bodyContent.js
@@ -91,7 +91,8 @@ function readyTopPageActive(){
 	});
 }
 
-function getNoticeList(){
+// API 서버에서 공지사항 리스트를 가져오는 함수
+function getNoticeList() {
 	let url;
 
 	url = apiServer + "/api/notice";
@@ -99,16 +100,47 @@ function getNoticeList(){
 	$.ajax({
 		"url": url,
 		"method": "get",
-		"dataType":"json",
+		"dataType": "json",
 		"cache": false,
-		success:(data) => {
+		success: (data) => {
 			let list;
-			if(data.result === "ok"){
+			let listjson;
+			if (data.result === "ok") {
 				list = cipher.decAes(data.data);
-				list = JSON.parse(list);
-			}else{
-				alert("?");
+				listjson = JSON.parse(list);
+				drawNoticeList(listjson);
+				console.log(listjson);
+			} else {
+				msg.set("등록된 공지사항이 없습니다");
 			}
 		}
 	})
 } // End of getNoticeList()
+
+
+// API 서버에서 가져온 공지사항의 리스트를 화면에 그리는 함수
+function drawNoticeList(listjson) {
+
+	let html, target, header, style;
+
+
+	target = $("#bodyContent");
+	style = "display:grid;grid-template-columns:20% 40% 20% 20%";
+	header = "<div style=\"" + style + "\"><div>번호</div><div>제목</div><div>작성자</div><div>등록일</div></div>";
+	body = "";
+	html = "";
+
+	for (let i in listjson) {
+		let no = listjson[i].no;
+		let title = listjson[i].title;
+		let writer = listjson[i].writer;
+		let created = listjson[i].created;
+		body += ("<div style=\"" + style + "\">");
+		body += ("<div>" + no + "</div>" + "<div>" + title + "</div>" + "<div>" + writer + "</div>" + "<div>" + created + "</div>");
+		body += "</div>";
+	}
+
+	html = (header + body);
+	if (target !== undefined && target !== undefined) target.html(html);
+}; // End of drawNoticeList()
+
