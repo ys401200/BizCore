46c9d10df97583d4aaa81cee4fd4b525f621d966
5079daeng <myy32612@gmail.com>
2022-07-22 15:25:38 +0900

    자료실 글 insert

--- src/main/java/kr/co/bizcore/v1/mapper/BoardMapper.java
+++ src/main/java/kr/co/bizcore/v1/mapper/BoardMapper.java
@@ -3,6 +3,7 @@ package kr.co.bizcore.v1.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import kr.co.bizcore.v1.domain.Article;
@@ -11,20 +12,20 @@ import kr.co.bizcore.v1.domain.SimpleArticle;
 
 public interface BoardMapper {
 
-    @Select("SELECT no, writer, title, IFNULL(modified, created) AS created FROM filebox WHERE deleted IS NULL AND comp_id = #{compId} ORDER BY no")
+    @Select("SELECT no, writer, title, IFNULL(modified, created) AS created FROM bizcore.filebox WHERE deleted IS NULL AND comp_id = #{compId} ORDER BY no")
     public List<SimpleArticle> getFileboxList(String compId);
 
-    @Select("SELECT no, writer, title, content, created, modified FROM filebox WHERE no = #{no} AND comp_id = #{compId} AND deleted IS NULL")
+    @Select("SELECT no, writer, title, content, created, modified FROM bizcore.filebox WHERE no = #{no} AND comp_id = #{compId} AND deleted IS NULL")
     public Article getFileboxArticle(int no, String compId);
 
-    @Select("SELECT idx, article_no AS articleNo, ogn_name AS ognName, created, modified, size, removed FROM filebox_attached WHERE article_no = #{articleNo} AND comp_id = #{compId}")
+    @Select("SELECT idx, article_no AS articleNo, ogn_name AS ognName, created, modified, size, removed FROM bizcore.filebox_attached WHERE article_no = #{articleNo} AND comp_id = #{compId}")
     public List<AttachedFile> getAttachedFileList(int articleNo, String compId);
 
-    @Select("SELECT filebox_next_no(#{compId})")
+    @Select("SELECT bizcore.filebox_next_no(#{compId})")
     public int getNewFileboxNo(String compId);    
 
-    @Insert("INSERT INTO filebox(comp_id, no, writer, title, content) VALUES(#{compId}, #{article.no}, #{article.writer}, #{article.title}, #{article.content})")
-    public void insertNewFileboxArticle(String compId, Article article);
+    @Insert("INSERT INTO bizcore.filebox(comp_id, no, writer, title, content) VALUES(#{compId}, #{article.no}, #{article.writer}, #{article.title}, #{article.content})")
+    public void insertNewFileboxArticle(@Param("compId") String compId, @Param("article")Article article);
 
     @Insert("INSERT INTO filebox_attached(comp_id, article_no, ogn_name, saved_name, size) VALUES()")
     public void addFileboxAttachedFile(String compId, int articleNo, String ognName, String savedName, long size);
--- src/main/resources/static/js/businessFilebox/businessFilebox.js
+++ src/main/resources/static/js/businessFilebox/businessFilebox.js
@@ -11,20 +11,19 @@ $(document).ready(() => {
 
 
 function drawFileForm() {
-     let target , html, fileInput;
+	let target, html, fileInput;
+	target = $(".fileboxList");
 
-     target = $(".fileboxList"); 
+	html = "<div style='display:grid;grid-template-columns:10% 90%'><label class='labelClass'>제목</label><input type='text' class='titleClass'/></div>" +
+		"<div style='display:grid;grid-template-columns:10% 90%' class='contentDiv'><label class='labelClass'>내용</label><textarea style='resize : none;'  class='contentClass' ></textarea></div>" +
+		"<div style='display:grid;grid-template-columns:10% 90%'><label class='labelClass'>첨부파일</label><input type='file' multiple class='fileClass'/></div>" +
+		"<div class='attachedFileName'></div>" +
+		"<div class='buttonDiv'><button type='button' class='uploadFileBoard' onclick='uploadFileBoard()'>등록</button><button type='button'>취소</button></div>";
 
-     html="<div style='display:grid;grid-template-columns:10% 90%'><label class='labelClass'>제목</label><input type='text' class='titleClass'/></div>"+
-     "<div style='display:grid;grid-template-columns:10% 90%' class='contentDiv'><label class='labelClass'>내용</label><textarea style='resize : none;' class='contentClass'></textarea></div>"+
-     "<div style='display:grid;grid-template-columns:10% 90%'><label class='labelClass'>첨부파일</label><input type='file' multiple class='fileClass'/></div>"+
-     "<div class='attachedFileName'></div>"+
-     "<div class='buttonDiv'><button type='button'>등록</button><button type='button'>취소</button></div>";
-    
-    target.html(html);  
+	target.html(html);
 
-    fileInput = document.getElementsByClassName('fileClass');
-    fileInput[0].addEventListener('change',getInputTag);
+	fileInput = document.getElementsByClassName('fileClass');
+	fileInput[0].addEventListener('change', getInputTag);
 }
 
 
@@ -32,42 +31,46 @@ function drawFileForm() {
 //파일 db에 파일 올리는 함수 // 제일 최근에 올린 파일을 게시글에 첨부? 
 function getInputTag() {
 
-let fileInput, input , file  =  null;
+	let fileInput, input, file = null;
 
 	fileInput = document.getElementsByClassName('fileClass');
-    input = fileInput[0];
-    if(input !== undefined && input !== null) file = input.files[0];
-   getfileBinary(file); 
-   
-        
+	input = fileInput[0];
+	if (input !== undefined && input !== null) file = input.files[0];
+	getfileBinary(file);
+
+
 } //End of getInputTag(); 
 
 
 function getfileBinary(file) {
-let reader;
+	let reader;
 
-if(file !== undefined && file !== null) {
-            reader = new FileReader();
+	if (file !== undefined && file !== null) {
+		reader = new FileReader();
 
-            reader.onload = (e) => {
-                let fileData = e.target.result;
-                fileData= cipher.encAes(fileData);
-                let fullData = (file.name + "\r\n" + fileData); // 파일 제목과 파일 내용 
-                
-                console.log(fullData);
-                submitFile(fullData)
-            }
+		reader.onload = (e) => {
+			let fileData = e.target.result;
+			fileData = cipher.encAes(fileData);
+			let fullData = (file.name + "\r\n" + fileData); // 파일 제목과 파일 내용 
 
-           reader.readAsBinaryString(file);
-        }
+			console.log(fullData);
+			submitFile(fullData)
+		}
+
+		reader.readAsBinaryString(file);
+	}
 
 }//End of getfileBinary(file); 
+
+
+
+
 
 ////////////// 파일 다중 선택한 경우 ? 
 
 // function getMultipleFileBinary(file) {
 //     let reader ; 
-   
+
 //     for(let i = 0 ; i < file.length; i ++) {
 //        reader = new FileReader(); 
 //        reader.onload = (e) => {
@@ -75,30 +78,37 @@ if(file !== undefined && file !== null) {
 //        let title = file[i].name;
 //        console.log(title + filedata+"////////////"); 
 //        }
-      
+
 //        reader.readAsBinaryString(file[i]);
-       
+
 //     } 
 
 // }
 
 
-function submitFile(fullData){
-    let url , target ;
-    target = $(".attachedFileName");
+
+
+
+
+// 첨부파일 임시로 올리는 함수 
+function submitFile(fullData) {
+	let url, target, title, fullDataSplit;
 	url = apiServer + "/api/board/filebox/attached";
+	target = $(".attachedFileName");
+
+	fullDataSplit = fullData.split("\r\n");
+	title = fullDataSplit[0];
 
 	$.ajax({
 		"url": url,
 		"method": "post",
-		"data" : fullData,
-        "contentType" : "text/plain",
-        "dataType" : "json",
+		"data": fullData,
+		"contentType": "text/plain",
+		"dataType": "json",
 		"cache": false,
 		success: (result) => {
 			if (result.result === "ok") {
-				target.html(result.msg); 
-				
+				drawTempFileList(title);
 			} else {
 				modal.alert("파일 업로드를 실패했습니다.");
 			}
@@ -106,271 +116,126 @@ function submitFile(fullData){
 	})
 }// End of submitFile(fileData); 
 
-function getFileBoardList() {
-	let url; 
-	url = apiServer + "/api/board/filebox" ; 
-	let fileBoardList ; 
+
+
+
+
+
+//임시로 올라간 첨부파일 목록 그리는 함수 
+function drawTempFileList(title) {
+	let target, html, titleSplit, format, img;
+
+	target = $(".attachedFileName");
+	html = target.html();
+	titleSplit = title.split(".");
+	format = titleSplit[1];
+
+	if (format === 'xls' || format === 'xlsx') {
+		//img = "<img src='/images/xls.jpg'/>";
+	}
+	else if (format === 'hwp') {
+		//img = "<img src='/images/hwp.jpg'/>";
+	}
+	else if (format === 'pdf') {
+		//img = "<img src='/images/pdf.jpg'/>";
+	}
+	else if (format === 'jpg' || format === 'jpeg' || format === 'png' || format === 'bmp') {
+		//	img = "<img src='/images/image.jpg'/>";
+	}
+	else if (format === 'txt') {
+		//img = "<img src='/images/text.jpg'/>";
+	}
+	else if (format === 'ppt') {
+		//img = "<img src='/images/ppt.jpg'/>";
+	}
+	else if (format === 'zip') {
+		//img ="<img src='/images/zip.jpg'/>";
+	}
+
+	html += ("<div style='display:grid;grid-template-columns: 10% 45% 45%' class='eachFileDiv'>" + img + "<div>" + title + "</div><button type='button' class='fileDelete' onclick='deleteParentDiv(this)'>×</button></div>");
+
+	target.html(html);
+
+
+} // End of drawTempFileList(title);
+
+
+
+
+
+
+//delete 버튼 눌러서 목록에서 지우는 함수 
+function deleteParentDiv(obj) {
+	let parent;
+	parent = obj.parentNode;
+	parent.remove();
+
+}
+
+
+
+
+// 자료실 글 insert 하는 함수 
+function uploadFileBoard() {
+	let url, title, content ,data;
+	let fileDivs = [];
+	title = $(".titleClass").val();
+	content = $(".contentClass").val();
+
+	if (title === null || title === "" || title === undefined) {
+		modal.alert("알림", "제목을 입력하세요");
+
+	} else if (content === null || content === "" || content === undefined) {
+		modal.alert("알림", "내용을 입력하세요");
+	}
+
+
+	 gettempFileListArr(fileDivs);
+
+	url = apiServer + "/api/board/filebox";
+
+
+    data = {
+			"title": title,
+			"content": content,
+			"files": fileDivs
+		};
+		data=JSON.stringify(data)
+		data = cipher.encAes(data);
+		///////////// JSON
+//JSON.stringify(data) application/json          data text/plain 
 	$.ajax({
+
 		"url": url,
-		"method": "get",
-		"dataType" : "json",
+		"method": "post",
+		"data": data,
+		"contentType" : "application/json " ,
+		"dataType": "json",
 		"cache": false,
 		success: (result) => {
 			if (result.result === "ok") {
-			
+				modal.alert("알림","자료실 글을 등록하는데 성공 했습니다 ");
 			} else {
-				modal.alert("자료실 글을 읽어오는데 실패했습니다 "); 
+				modal.alert("알림","자료실 글을 등록하는데 실패했습니다 ");
 			}
 		}
-	});
-}
+	})
 
-// function getAttachedFileNames() {
-// let target = $(".attachedFileName");
-// let html ="";
-// 	let fileInput = document.getElementsByClassName('fileClass');
-// 	for ( let i = 0; i < fileInput.length ; i++ ) {
-// 		if(fileInput[i].files.length >0) {
-// 			for(let j = 0; j <fileInput[i].files.length; j++){
-// 				console.log(fileInput[i].files[j].name);
-// 				html += ( (j+1)+"번 파일 :"+ fileInput[i].files[j].name +",");
-// 			}
-// 		}
-// 	}
-    
+
+} // End of uploadFileBoard(); 
 
 
 
-//     function getFileDetail(){
-//       let fileData;  let file, fileEl, reader;
-// 	  let fileInput = document.getElementsByClassName('inputClass');
-//         fileEl = fileInput[0];
-//         if(fileEl !== undefined && fileEl !== null) file = fileEl.files[0];
-
-//         if(file !== undefined && file !== null){
-//             reader = new FileReader();
-//             reader.onload = (e) => {
-//                 fileData = e.target.result;
-
-// 				console.log(fileData);
-//                 $('.fileDetail').html(fileData);
-//             }
-//             reader.readAsBinaryString(file);
-//         }
-//     }
-
-
-
-// // API 서버에서 공지사항 리스트를 가져오는 함수
-// function getFileboxList() {
-// 	let url;
-// 	url = apiServer + "/api/filebox"
-// 	$.ajax({
-// 		"url": url,
-// 		"method": "get",
-// 		"dataType": "json",
-// 		"cache": false,
-// 		success: (data) => {
-// 			let jsonData;
-// 			if (data.result === "ok") {
-// 				jsonData = cipher.decAes(data.data);
-// 				jsonData = JSON.parse(jsonData);
-// 				storage.fileboxList = jsonData;
-// 				window.setTimeout(drawFileboxList, 200);
-// 			} else {
-// 				modal.alert("등록된 자료가 없습니다");
-// 			}
-// 		}
-// 	})
-// } // End of getFileboxList()
-
-// function drawFileboxList() {
-// 	let container;
-// 	let jsonData;
-// 	let header = [];
-// 	let data = [];
-// 	let ids = [];
-// 	let disDate, setDate, str, fnc;
-// 	let totalNotice, currentPage, articlePerPage, max;
-// 	let lastPageNotice; 
-// 	if (storage.fileboxList === undefined) {
-// 		modal.alert("등록된 자료가 없습니다");
-// 	}
-// 	else {
-// 		jsonData = storage.fileboxList;
-// 	}
-// 	if (storage.currentPage === undefined) storage.currentPage = 1;
-// 	if (storage.articlePerPage === undefined) storage.articlePerPage = 5;
-
-// 	currentPage = storage.currentPage;
-// 	articlePerPage = storage.articlePerPage;
-// 	totalNotice = jsonData.length;
-// 	max = Math.ceil(totalNotice / articlePerPage);
-
-// 	pageContainer = document.getElementsByClassName("pageContainer");
-// 	container = $(".gridNoticeList");
-
-
-// 	header = [
-// 		{
-// 			"title": "번호",
-// 			"padding": false,
-// 		},
-// 		{
-// 			"title": "제목",
-// 			"padding": true,
-// 		},
-// 		{
-// 			"title": "작성자",
-// 			"padding": false,
-// 		},
-// 		{
-// 			"title": "등록일",
-// 			"padding": false,
-// 		}
-// 	];
-//} // End of getFileboxList()
-
-function drawFileboxList() {
-	let container;
-	let jsonData;
-	let header = [];
-	let data = [];
-	let ids = [];
-	let disDate, setDate, str, fnc;
-	let totalNotice, currentPage, articlePerPage, max;
-	let lastPageNotice; 
-	if (storage.fileboxList === undefined) {
-		modal.alert("등록된 자료가 없습니다");
+// 자료실 게시글과 같이(올릴 첨부파일 목록 배열 만드는 함수 
+function gettempFileListArr(fileDivs) {
+	let target = $(".eachFileDiv");
+	let title;
+	for (let i = 0; i < target.length; i++) {
+		title = target[i].children[0].innerText;
+		fileDivs.push(title);
 	}
-	else {
-		jsonData = storage.fileboxList;
-	}
-	if (storage.currentPage === undefined) storage.currentPage = 1;
-	if (storage.articlePerPage === undefined) storage.articlePerPage = 5;
 
-	currentPage = storage.currentPage;
-	articlePerPage = storage.articlePerPage;
-	totalNotice = jsonData.length;
-	max = Math.ceil(totalNotice / articlePerPage);
-
-	pageContainer = document.getElementsByClassName("pageContainer");
-	container = $(".gridNoticeList");
-
-
-	header = [
-		{
-			"title": "번호",
-			"align": "center",
-		},
-		{
-			"title": "제목",
-			"align": "left",
-		},
-		{
-			"title": "작성자",
-			"align": "center",
-		},
-		{
-			"title": "등록일",
-			"align": "center",
-		}
-	];
-}
-// 	lastPageNotice =currentPage*articlePerPage;
-
-// 	//마지막 페이지인 경우 게산 
-// 	if(currentPage==max && totalNotice%articlePerPage !==0){
-// 		lastPageNotice = (max-1)*articlePerPage + totalNotice%articlePerPage;
-// 	}
-
-// 	for (let i = (currentPage-1)*articlePerPage; i < lastPageNotice; i++) { 
-		
-// 		disDate = dateDis(jsonData[i].created, jsonData[i].modified);
-// 		setDate = dateFnc(disDate);
-// 		let userName = storage.user[jsonData[i].writer].userName;
-
-// 		str = [
-// 			{
-// 				"setData": jsonData[i].no,
-// 			},
-// 			{
-// 				"setData": jsonData[i].title,
-// 			},
-// 			{
-// 				"setData": userName,
-// 			},
-// 			{
-// 				"setData": setDate,
-// 			}
-// 		]
-
-// 		fnc = "fileboxDetailView(this)";
-// 		ids.push(jsonData[i].no);
-// 		data.push(str);
-
-		
-// 	}
-
-// 	let pageNation = createPaging(pageContainer[0], max, "pageMove", currentPage);
-// 	pageContainer[0].innerHTML = pageNation;
-// 	//표 만들기 
-// 	createGrid(container, header, data, ids, fnc);
-
-
-// }// End of drawFileboxList()
-
-
-// function pageMove(page) {
-// 	let selectedPage = parseInt(page);
-// 	storage.currentPage = selectedPage;
-// 	drawFileboxList();
-	
-// }
+}// End of gettempFileListArr(fileDivs);
 
 
 
-// function fileboxDetailView(event) {// 선택한 그리드의 글 번호 받아오기 
-// 	let no = event.dataset.id;
-// 	let url;
-// 	url = apiServer + "/api/filebox/" + no;
-
-
-// 	$.ajax({
-// 		"url": url,
-// 		"method": "get",
-// 		"dataType": "json",
-// 		"cache": false,
-// 		success: (result) => {
-// 			let jsonData;
-// 			if (result.result === "ok") {
-// 				jsonData = cipher.decAes(result.data);
-// 				jsonData = JSON.parse(jsonData);
-// 				drawFileboxContent(jsonData);
-// 			} else {
-// 				modal.alert("자료 상세조회에 실패했습니다.");
-// 			}
-// 		}
-// 	})
-
-// } // End of noticeDetailView()
-
-// function drawFileboxContent(jsonData) { // 공지사항 본문 보이게 하는 함수 
-// 	let title = jsonData.title;
-// 	let content = jsonData.content;
-// 	let html = "";
-// 	let headerDiv;
-// 	let contentDiv;
-
-// 	headerDiv = "<div class='headerDiv' style='display:grid;grid-template-columns:80% 20%' onclick='deleteNoticeContent()'><div>" + title + "</div><div class='deleteButton'>X</div></div>";
-// 	contentDiv = "<div class='contentDiv' style='display:grid'>" + content + "</div>";
-// 	html += (headerDiv + contentDiv);
-// 	$(".fileboxContent").html(html);
-// 	$(".fileboxContent").show();
-
-
-// }// End of drawNoticeContent()
-
-
-// function deleteNoticeContent() { // 공지사항 본문 지우는 함수 
-// 	$(".fileboxContent").hide();}
