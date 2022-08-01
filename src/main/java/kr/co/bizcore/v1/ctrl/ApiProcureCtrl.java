fd21e47b4d988c68bc2c0a2aefbde4c1e6b913d9
jo-beoungil <enky001@gmail.com>
2022-08-01 09:56:23 +0900

    API 조달 post, put, delete 추가

--- src/main/java/kr/co/bizcore/v1/ctrl/ApiProcureCtrl.java
+++ src/main/java/kr/co/bizcore/v1/ctrl/ApiProcureCtrl.java
@@ -1,19 +1,24 @@
 package kr.co.bizcore.v1.ctrl;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.bizcore.v1.domain.Procure;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/procure")
+@RequestMapping("/a[i/procure")
 @Slf4j
 public class ApiProcureCtrl extends Ctrl{
 
@@ -43,19 +48,18 @@ public class ApiProcureCtrl extends Ctrl{
                 result = "{\"result\":\"ok\",\"data\":\"" + list + "\"}";
             }
         return result;
-    } // End of apiProcure
+    } // End of apiProcureGet
 
-    @RequestMapping(value = "/{no}", method = RequestMethod.GET)
-    public String apiProcureNumberGet(HttpServletRequest request, @PathVariable String no){
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String apiProcurePost(HttpServletRequest request, @RequestBody String requestBody){
         String result = null;
         String compId = null;
         String aesKey = null;
         String aesIv = null;
         String data = null;
-        int number = -1;
+        JSONObject json = null;
+        Procure procure = null;
         HttpSession session = null;
-
-        number = procureService.strToInt(no);
 
         session = request.getSession();
         compId = (String)session.getAttribute("compId");
@@ -65,21 +69,115 @@ public class ApiProcureCtrl extends Ctrl{
 
         if(compId == null){
             result = "{\"result\":\"failure\",\"msg\":\"Company ID is Not verified.\"}";
-        }else if(number < 0){
-            result = "{\"result\":\"failure\",\"msg\":\"Sopp number is invalid.\"}";
         }else if(aesKey == null || aesIv == null){
             result = "{\"result\":\"failure\",\"msg\":\"Encryption key is not set.\"}";
         }else{
-            data = procureService.getProcure(number, compId);
+            data = procureService.decAes(requestBody, aesKey, aesIv);
             if(data == null){
-                result = "{\"result\":\"failure\",\"msg\":\"Sales not exist.\"}";
+                result = "{\"result\":\"failure\",\"msg\":\"Decryption fail.\"}";
             }else{
-                data = soppService.encAes(data, aesKey, aesIv);
-                result = "{\"result\":\"ok\",\"data\":\"" + data + "\"}";
+                procure = new Procure();
+                json = new JSONObject(data);
+                procure.setBuyerArea(json.getString("buyerArea"));
+                procure.setBuyerAreaCode(json.getString("buyerAreaCode"));
+                procure.setBuyerCode(json.getString("buyerCode"));
+                procure.setBuyerName(json.getString("buyerName"));
+                procure.setContractDate(new Date(strToLong(json.getString("contractDate"))));
+                procure.setDeliveryDate(new Date(strToLong(json.getString("DeliveryDate"))));
+                procure.setDeliveryPlace(json.getString("deliveryPlace"));
+                procure.setItemAmount(json.getLong("itemAmount"));
+                procure.setItemNetPrice(json.getInt("itemNetPrice"));
+                procure.setItemQty(json.getInt("itemQty"));
+                procure.setItemUnit(json.getString("itemUnit"));
+                procure.setModAmount(json.getLong("modAmount"));
+                procure.setModQty(json.getInt("modQty"));
+                procure.setRequestItem(json.getString("requestItem"));
+                procure.setRequestItemCode(json.getString("requestItemCode"));
+                procure.setRequestNo(json.getString("requestNo"));
+                procure.setSopp(json.getInt("sopp"));
+                procure.setTitle(json.getString("title"));
+                if(procureService.addProcure(procure, compId))  result = "{\"result\":\"ok\"}";
+                else                                            result = "{\"result\":\"failure\",\"msg\":\"An error occurred\"}";
             }
         }
 
         return result;
-    } // End of apiProcure
+    } // End of apiProcurePost()
+
+    @RequestMapping(value = "/{no}", method = RequestMethod.PUT)
+    public String apiProcureNumberPut(HttpServletRequest request, @RequestBody String requestBody, @PathVariable String no){
+        String result = null;
+        String compId = null;
+        String aesKey = null;
+        String aesIv = null;
+        String data = null;
+        JSONObject json = null;
+        Procure procure = null;
+        HttpSession session = null;
+
+        session = request.getSession();
+        compId = (String)session.getAttribute("compId");
+        aesKey = (String)session.getAttribute("aesKey");
+        aesIv = (String)session.getAttribute("aesIv");
+        if(compId == null)  compId = (String)request.getAttribute("compId");
+
+        if(compId == null){
+            result = "{\"result\":\"failure\",\"msg\":\"Company ID is Not verified.\"}";
+        }else if(aesKey == null || aesIv == null){
+            result = "{\"result\":\"failure\",\"msg\":\"Encryption key is not set.\"}";
+        }else{
+            data = procureService.decAes(requestBody, aesKey, aesIv);
+            if(data == null){
+                result = "{\"result\":\"failure\",\"msg\":\"Decryption fail.\"}";
+            }else{
+                procure = new Procure();
+                json = new JSONObject(data);
+                procure.setNo(strToInt(no));
+                procure.setBuyerArea(json.getString("buyerArea"));
+                procure.setBuyerAreaCode(json.getString("buyerAreaCode"));
+                procure.setBuyerCode(json.getString("buyerCode"));
+                procure.setBuyerName(json.getString("buyerName"));
+                procure.setContractDate(new Date(strToLong(json.getString("contractDate"))));
+                procure.setDeliveryDate(new Date(strToLong(json.getString("DeliveryDate"))));
+                procure.setDeliveryPlace(json.getString("deliveryPlace"));
+                procure.setItemAmount(json.getLong("itemAmount"));
+                procure.setItemNetPrice(json.getInt("itemNetPrice"));
+                procure.setItemQty(json.getInt("itemQty"));
+                procure.setItemUnit(json.getString("itemUnit"));
+                procure.setModAmount(json.getLong("modAmount"));
+                procure.setModQty(json.getInt("modQty"));
+                procure.setRequestItem(json.getString("requestItem"));
+                procure.setRequestItemCode(json.getString("requestItemCode"));
+                procure.setRequestNo(json.getString("requestNo"));
+                procure.setSopp(json.getInt("sopp"));
+                procure.setTitle(json.getString("title"));
+                if(procureService.addProcure(procure, compId))  result = "{\"result\":\"ok\"}";
+                else                                            result = "{\"result\":\"failure\",\"msg\":\"An error occurred\"}";
+            }
+        }
+
+        return result;
+    } // End of apiProcureNumberPut()
+
+    @RequestMapping(value = "/{no}", method = RequestMethod.DELETE)
+    public String apiProcureNumberDelete(HttpServletRequest request, @PathVariable String no){
+        String result = null;
+        String compId = null;
+        HttpSession session = null;
+
+        session = request.getSession();
+        compId = (String)session.getAttribute("compId");
+        if(compId == null)  compId = (String)request.getAttribute("compId");
+
+        if(compId == null){
+            result = "{\"result\":\"failure\",\"msg\":\"Company ID is Not verified.\"}";
+        }else{
+            if(procureService.removeProcure(no, compId))    result = "{\"result\":\"ok\"}";
+            else                                            result = "{\"result\":\"failure\",\"msg\":\"An error occurred\"}";
+        }
+
+        return result;
+    } // End of apiProcureNumberDelete()
     
 }
+
