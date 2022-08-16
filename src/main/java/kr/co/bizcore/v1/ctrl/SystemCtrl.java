1cb672c6da5990c3d9424250f8808e3f3c88d1dd
jo-beoungil <enky001@gmail.com>
2022-08-16 16:23:31 +0900

    전자결재 컨버팅 임시프로그램 추가

--- src/main/java/kr/co/bizcore/v1/ctrl/SystemCtrl.java
+++ src/main/java/kr/co/bizcore/v1/ctrl/SystemCtrl.java
@@ -2,15 +2,22 @@ package kr.co.bizcore.v1.ctrl;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.bizcore.v1.domain.Sopp;
 import kr.co.bizcore.v1.domain.TradeDetail;
+import kr.co.bizcore.v1.svc.SoppService;
+import kr.co.bizcore.v1.svc.TestService;
 import kr.co.bizcore.v1.svc.TradeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,27 +29,73 @@ public class SystemCtrl extends Ctrl {
     private static final Logger logger = LoggerFactory.getLogger(SystemCtrl.class);
 
     @Autowired
-    private TradeService svc;
+    private SoppService svc;
+
+    @Autowired
+    private TestService tsvc;
 
     @RequestMapping(value = "/connUrl", method = RequestMethod.GET)
     public String connUrl() {
         return systemService.getConnUrl();
     } // End of connUrl()
 
-    @GetMapping("/test")
-    public String test(){
+    @GetMapping("/test/{no}")
+    public String test(@PathVariable String no){
         String result = null;
 
-        TradeDetail e1 = null, e2 = null;
-        e1 = svc.getTradeDetail(1825+"");
-        e2 = svc.getTradeDetail(1828+"");
+        Sopp e1 = null, e2 = null;
+        e1 = svc.getSopp(no, "vtek");
+        e2 = svc.getSopp(10005598 + "", "vtek");
 
         
 
         //result = e1.createInsertQuery(null, null);
-        result = e1.createUpdateQuery(e2, null);
+        if(e1 == null)  result = "null";
+        else{
+            result = e1.createInsertQuery(null, "vtek");
+            result = "result" + svc.executeSqlQuery(result);
+        }
 
         return result;
     } // End of test()
+
+    @GetMapping("/test/file")
+    public String file(){
+        tsvc.dbToStorageGwFile();
+        return "00000";
+    } // End of test()
+
+    @GetMapping("/doc")
+    public String getDocList(HttpServletRequest request, HttpServletResponse response){
+        String result = null;
+
+        response.setContentType("application/json;charset=utf-8");
+
+        result = tsvc.docList();
+
+        if(result == null)  result = "{\"result\":\"failure\",\"msg\":\"Data is null.\"}";
+        else        result = "{\"result\":\"ok\",\"data\":" + result + "}";
+
+
+
+        return result;
+    }
+
+    @GetMapping("/doc/{docNo}")
+    public String getDocData(HttpServletRequest request, HttpServletResponse response, @PathVariable String docNo){
+        String result = null, data1 = null, data2 = null, data3 = null;
+
+        response.setContentType("application/json;charset=utf-8");
+
+        data1 = tsvc.docFile(docNo);
+        data2 = tsvc.docData(docNo);
+        data3 = tsvc.docApp(docNo);
+
+        result = "{\"result\":\"ok\",\"files\":" + (data1 == null ? "null" : data1) + ",\"docData\":" + (data2 == null ? "null" : data2) + ",\"appData\":" + (data3 == null ? "null" : data3) + "}";
+
+
+
+        return result;
+    }
 
 }
--- src/main/java/kr/co/bizcore/v1/svc/TestService.java
+++ src/main/java/kr/co/bizcore/v1/svc/TestService.java
@@ -1,9 +1,14 @@
 package kr.co.bizcore.v1.svc;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.springframework.stereotype.Service;
@@ -35,5 +40,266 @@ public class TestService extends Svc{
 
         return result;
     }
+
+    // 전자결재 문서의 첨부파일을 DB에서 다운받아서 저장하는 메서드
+    public void dbToStorageGwFile(){
+        String sql1 = "SELECT fileid FROM swcore.swc_businessfiledata WHERE attrib NOT LIKE 'XXX%' AND filename <> ''";
+        String sql2 = "SELECT filename, uploaddate, filecontent, docno FROM swcore2.swc_businessfiledata WHERE fileid = ?";
+        String dirPath = null;
+        String fileName = null;
+        String rootPath = "/Users/jbi/Desktop/bizUpload/vtek/appDoc/";
+        File file = null;
+        File dir = null;
+        Blob blob = null;
+        byte[] buffer = new byte[1024];
+        InputStream is = null;
+        FileOutputStream fos = null;
+        long fileDate = 0;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        ArrayList<String> ids = new ArrayList<>();
+        int delete = 0, create = 0, x = 0, length = -1;
+
+        try{
+            conn = sqlSession.getConnection();
+            pstmt = conn.prepareStatement(sql1);
+            rs = pstmt.executeQuery();
+            while(rs.next()){
+                ids.add(rs.getString(1));
+            }
+
+            // DB의 파일 ID를 가져온 후 이를 표현하고 다음 작업을 준비함
+            System.out.println("[TEST SVC] ::::: get File ID's count : " + ids.size());
+            if(rs != null)  rs.close();
+            if(pstmt != null)   pstmt.close();
+
+            for(x = 0 ; x < ids.size() ; x++){
+
+                // HTTP 타임아웃 방지
+                if(x <= 248) continue;
+
+                pstmt = conn.prepareStatement(sql2);
+                pstmt.setString(1, ids.get(x));
+                rs = pstmt.executeQuery();
+                if(rs.next()){
+                    fileName = rs.getString(1);
+                    fileDate = rs.getDate(2).getTime();
+                    fileDate += rs.getTime(2).getTime();
+                    dirPath = rs.getInt(4) + "";
+                    dir = new File(rootPath + dirPath);
+                    if(!dir.exists())   dir.mkdirs();
+                    file = new File(rootPath + dirPath + "/" + fileName);
+                    if(file.exists() && file.delete()) delete++;
+
+                    System.out.println("[TEST SVC] count : " + x + " / create : " + create + " / delete : " + delete + " / docno : " + dirPath);
+
+                    blob = rs.getBlob(3);
+                    is = blob.getBinaryStream();
+                    fos = new FileOutputStream(file);
+                    while((length = is.read(buffer)) != -1){
+                        fos.write(buffer, 0, length);
+                    }
+                    file.setLastModified(fileDate);
+                    fos.close();
+                    is.close();
+                    create++;
+                }
+                if(rs != null)  rs.close();
+                if(pstmt != null)   pstmt.close();
+                //System.out.println("[TEST SVC] count : " + x + " / create : " + create + " / delete : " + delete + " / docno : " + dirPath);
+            }
+
+        }catch(Exception e){e.printStackTrace();}finally{
+            try{
+                if(rs != null)  rs.close();
+                if(pstmt != null)   pstmt.close();
+                if(conn != null)    conn.close();
+            }catch(Exception e){System.out.println("[ERROR] ===== Closing Error =====");e.printStackTrace();}
+        }
+    } 
+
+    public String docList(){
+        String result = null;
+        String sql1 = "SELECT docno AS no, doccruserno AS writer, doctype AS type, doctitle AS title, docstatus AS status, linksoppno AS sopp, linkcustno AS customer, docdesc AS content, docformno AS form, docdrawstatus AS draw, docdate AS created FROM swcore.swc_businessdoc WHERE attrib NOT LIKE 'XXX%' ORDER BY docno DESC";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String xType = null, xTitle = null, xContent = null, xForm = null;
+        Date xDate = null;
+        int xNo = -1, xWriter = -1, xStatus = -1, xSopp = -1, xCustomer = -1, xDraw = -1;
+
+        try{
+            conn = sqlSession.getConnection();
+            pstmt = conn.prepareStatement(sql1);
+            rs = pstmt.executeQuery();
+
+            while(rs.next()){
+                xNo = rs.getInt(1);
+                xWriter = rs.getInt(2);
+                xType = rs.getString(3);
+                xTitle = rs.getString(4);
+                xStatus = rs.getInt(5);
+                xSopp = rs.getInt(6);
+                xCustomer = rs.getInt(7);
+                xContent = rs.getString(8);
+                xForm = rs.getString(9);
+                xDraw = rs.getInt(10);
+                xDate = rs.getDate(11);
+
+                if(result == null)  result = "[";
+                else                result += ",";
+
+                result += ("{\"no\":" + xNo + ",");
+                result += ("\"writer\":" + xWriter + ",");
+                result += ("\"type\":" + (xType == null ? "null" : "\"" + xType + "\"") + ",");
+                result += ("\"title\":" + (xTitle == null ? "null" : "\"" + xTitle + "\"") + ",");
+                result += ("\"status\":" + xStatus + ",");
+                result += ("\"sopp\":" + xSopp + ",");
+                result += ("\"customer\":" + xCustomer + ",");
+                result += ("\"content\":" + (xContent == null ? "null" : "\"" + xContent.replaceAll("\"", "\\u0022").replaceAll("\t", "\\u0022") + "\"") + ",");
+                result += ("\"form\":" + (xForm == null ? "null" : "\"" + xForm + "\"") + ",");
+                result += ("\"draw\":" + xDraw + ",");
+                result += ("\"created\":" + ( xDate == null ? "null" : xDate.getTime()) + "}");
+
+
+                xType = null;
+                xTitle = null;
+                xContent = null;
+                xForm = null;
+                xDate = null;
+                xNo = -1;
+                xWriter = -1;
+                xStatus = -1;
+                xSopp = -1;
+                xCustomer = -1;
+                xDraw = -1;
+            }
+            if(result != null)  result += "]";
+        }catch(Exception e){e.printStackTrace();}
+
+        return result;
+    }
+
+    public String docData(String docNo){
+        String result = null;
+        String sql1 = "SELECT custname AS customer, productname AS product, productnetprice AS price, productqty AS qty, productamount AS subtotal, productvat AS tax, producttotal AS total, productremark AS remark FROM swcore.swc_businessdocdata WHERE attrib NOT LIKE 'XXX%' AND docno = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String xCustomer = null, xProduct = null, xRemark = null;
+        int xQty = -1;
+        long xPrice = -1, xSubtotal = -1, xTax = -1, xTotal = -1;
+
+        try{
+            conn = sqlSession.getConnection();
+            pstmt = conn.prepareStatement(sql1);
+            pstmt.setString(1, docNo);
+            rs = pstmt.executeQuery();
+
+            while(rs.next()){
+                xCustomer = rs.getString(1);
+                xProduct = rs.getString(2);
+                xPrice = rs.getLong(3);
+                xQty = rs.getInt(4);
+                xSubtotal = rs.getLong(5);
+                xTax = rs.getLong(6);
+                xTotal = rs.getLong(7);
+                xRemark = rs.getString(8);
+
+                if(result == null)  result = "[";
+                else                result += ",";
+
+                result += ("{\"customer\":" + (xCustomer == null ? "null" : "\"" + xCustomer + "\"") + ",");
+                result += ("\"product\":" + (xProduct == null ? "null" : "\"" + xProduct + "\"") + ",");
+                result += ("\"price\":" + xPrice + ",");
+                result += ("\"quantity\":" + xQty + ",");
+                result += ("\"subTotal\":" + xSubtotal + ",");
+                result += ("\"tax\":" + xTax + ",");
+                result += ("\"total\":" + xTotal + ",");
+                result += ("\"remark\":" + (xRemark == null ? "null" : "\"" + xRemark + "\"") + "}");
+
+
+                xCustomer = null;
+                xProduct = null;
+                xRemark = null;
+                xQty = -1;
+                xPrice = -1;
+                xSubtotal = -1;
+                xTax = -1;
+                xTotal = -1;
+            }
+            if(result != null)  result += "]";
+        }catch(Exception e){e.printStackTrace();}
+
+        return result;
+    }
+
+    public String docFile(String docNo){
+        String result = null;
+        String sql1 = "SELECT fileName FROM swcore.swc_businessfiledata WHERE attrib NOT LIKE 'XXX%' AND docno = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String fileName = null;
+
+        try{
+            conn = sqlSession.getConnection();
+            pstmt = conn.prepareStatement(sql1);
+            pstmt.setString(1, docNo);
+            rs = pstmt.executeQuery();
+            
+            while(rs.next()){
+                fileName = rs.getString(1);
+                if(result == null)  result = "[";
+                else                result += ",";
+                result += fileName;
+            }
+            if(result != null)  result += "]";
+        }catch(Exception e){e.printStackTrace();}
+
+        return result;
+    }
     
+    public String docApp(String docNo){
+        String result = null;
+        String sql1 = "SELECT usernocr AS writer, usernois AS request, usernoapp AS app, appstatus AS status, issuedate AS issued, appdate AS approved, appcomment AS comment FROM swcore.swc_businessdocapp WHERE attrib NOT LIKE 'XXX%' AND docno = ? ORDER BY appid";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String xStatus = null, xComment = null;
+        Date issued = null, approved = null;
+        int xWriter = -1, xRequest = -1, xApp = -1;
+
+        try{
+            conn = sqlSession.getConnection();
+            pstmt = conn.prepareStatement(sql1);
+            pstmt.setString(1, docNo);
+            rs = pstmt.executeQuery();
+            
+            while(rs.next()){
+                xWriter = rs.getInt(1);
+                xRequest = rs.getInt(2);
+                xApp = rs.getInt(3);
+                xStatus = rs.getString(4);
+                issued = rs.getDate(5);
+                approved = rs.getDate(6);
+                xComment = rs.getString(7);
+
+                if(result == null)  result = "[";
+                else                result += ",";
+                
+                result += ("{\"writer\":" + xWriter + ",");
+                result += ("\"request\":" + xRequest + ",");
+                result += ("\"app\":" + xApp + ",");
+                result += ("\"status\":" + (xStatus == null ? "null" : "\"" + xStatus + "\"") + ",");
+                result += ("\"issued\":" + (issued == null ? "null" : issued.getTime() ) + ",");
+                result += ("\"approved\":" + (approved == null ? "null" : approved.getTime() ) + ",");
+                result += ("\"comment\":" + (xComment == null ? "null" : "\"" + xComment + "\"") + "}");
+            }
+            if(result != null)  result += "]";
+        }catch(Exception e){e.printStackTrace();}
+
+        return result;
+    }
 }
