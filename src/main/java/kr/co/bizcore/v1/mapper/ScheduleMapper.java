6724a9ff8ff928cd8e83a903b7761f6de6aa11fb
jo-beoungil <enky001@gmail.com>
2022-09-13 09:52:06 +0900

    변경 전 일정 부분 삭제

--- src/main/java/kr/co/bizcore/v1/mapper/ScheduleMapper.java
+++ src/main/java/kr/co/bizcore/v1/mapper/ScheduleMapper.java
@@ -2,27 +2,22 @@ package kr.co.bizcore.v1.mapper;
 
 import java.util.Date;
 import java.util.List;
-
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
-
-import kr.co.bizcore.v1.domain.Sched;
 import kr.co.bizcore.v1.domain.Schedule;
 import kr.co.bizcore.v1.domain.WorkReport;
 
-public interface ScheduleMapper {
-    
+public interface ScheduleMapper {    
 
-    @Select("SELECT a.* FROM (" + 
-        "SELECT 'etc' AS job, schedno AS no, userno AS user, custno AS cust, soppno AS sopp, schedtitle AS title, scheddesc AS detail, schedfrom AS \"from\", schedto AS \"to\", schedplace AS place, regdatetime AS created, modDatetime AS modified FROM swc_sched WHERE schedfrom < DATE_ADD(#{ymd}, INTERVAL 1 MONTH) AND schedto >= #{ymd} AND compno = (SELECT compno FROM swc_company WHERE compid =#{compId}) " +
-        "UNION ALL " +
-        "SELECT 'sales' AS job, salesno AS no, userno AS user, custno AS cust, soppno AS sopp, salestitle AS title, salesdesc AS detail, salesfrdatetime AS \"from\", salestodatetime AS \"to\", salesplace AS place, regdatetime AS created, modDatetime AS modified FROM swc_sales WHERE salesfrdatetime < DATE_ADD(#{ymd}, INTERVAL 1 MONTH) AND salestodatetime >= #{ymd} AND compno = (SELECT compno FROM swc_company WHERE compid =#{compId}) " +
-        "UNION ALL " +
-        "SELECT 'tech' AS job, techdno AS no, userno AS user, custno AS cust, soppno AS sopp, techdtitle as title, techddesc as detail, techdfrom AS \"from\", techdto AS \"to\", techdplace AS place, regdatetime AS created, modDatetime AS modified FROM swc_techd WHERE techdfrom < DATE_ADD(#{ymd}, INTERVAL 1 MONTH) AND techdto >= #{ymd} AND compno = (SELECT compno FROM swc_company WHERE compid =#{compId}) " +
-    ") a WHERE a.user IN (SELECT user_no FROM bizcore.user_dept WHERE dept_id IN (#{deptIn})) ORDER BY a.from, a.user, a.job")
-    public List<Sched> getSchedule(@Param("compId") String compId, @Param("ymd") String ym, @Param("deptIn") String deptIn);
+    //@Select("SELECT a.* FROM (" + 
+    //    "SELECT 'etc' AS job, schedno AS no, userno AS user, custno AS cust, soppno AS sopp, schedtitle AS title, scheddesc AS detail, schedfrom AS \"from\", schedto AS \"to\", schedplace AS place, regdatetime AS created, modDatetime AS modified FROM swc_sched WHERE schedfrom < DATE_ADD(#{ymd}, INTERVAL 1 MONTH) AND schedto >= #{ymd} AND compno = (SELECT compno FROM swc_company WHERE compid =#{compId}) " +
+    //    "UNION ALL " +
+    //    "SELECT 'sales' AS job, salesno AS no, userno AS user, custno AS cust, soppno AS sopp, salestitle AS title, salesdesc AS detail, salesfrdatetime AS \"from\", salestodatetime AS \"to\", salesplace AS place, regdatetime AS created, modDatetime AS modified FROM swc_sales WHERE salesfrdatetime < DATE_ADD(#{ymd}, INTERVAL 1 MONTH) AND salestodatetime >= #{ymd} AND compno = (SELECT compno FROM swc_company WHERE compid =#{compId}) " +
+    //    "UNION ALL " +
+    //    "SELECT 'tech' AS job, techdno AS no, userno AS user, custno AS cust, soppno AS sopp, techdtitle as title, techddesc as detail, techdfrom AS \"from\", techdto AS \"to\", techdplace AS place, regdatetime AS created, modDatetime AS modified FROM swc_techd WHERE techdfrom < DATE_ADD(#{ymd}, INTERVAL 1 MONTH) AND techdto >= #{ymd} AND compno = (SELECT compno FROM swc_company WHERE compid =#{compId}) " +
+    //") a WHERE a.user IN (SELECT user_no FROM bizcore.user_dept WHERE dept_id IN (#{deptIn})) ORDER BY a.from, a.user, a.job")
+    //public List<Schedule> getSchedule(@Param("compId") String compId, @Param("ymd") String ym, @Param("deptIn") String deptIn);
 
     // ↑ 일정 통합 전 기존 작업 내용
     // =================================================================================================
