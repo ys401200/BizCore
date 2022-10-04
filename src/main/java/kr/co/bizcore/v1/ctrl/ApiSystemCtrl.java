a9a3c1ec4cfe1523485251120ce179854870bc83
jo-beoungil <enky001@gmail.com>
2022-10-04 17:02:23 +0900

    영업목표 등록/수정 추가

--- src/main/java/kr/co/bizcore/v1/ctrl/ApiSystemCtrl.java
+++ src/main/java/kr/co/bizcore/v1/ctrl/ApiSystemCtrl.java
@@ -3,11 +3,13 @@ package kr.co.bizcore.v1.ctrl;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -207,10 +209,44 @@ public class ApiSystemCtrl extends Ctrl{
 
     // 영업목표를 설정하는 메서드
     @PostMapping("/goal/{year:\\d{4}}/{empNo:\\d+}")
-    public String apiSystemGoalYearUsernoPost(HttpServletRequest request, @PathVariable("year") int year, @PathVariable("empNo") int empNo){
+    public String apiSystemGoalYearUsernoPost(HttpServletRequest request, @RequestBody String requestBody, @PathVariable("year") int year, @PathVariable("empNo") int empNo){
         String result = null;
+        String compId = null;
+        String aesKey = null;
+        String aesIv = null;
+        String userNo = null;
+        String data = null;
+        JSONArray jarr = null;
+        long[] larr = null;
+        Msg msg = null;
+        HttpSession session = null;
+        int x = 0;
 
-        //"permissionDenied";
+        session = request.getSession();
+        msg = getMsg((String)session.getAttribute("lang"));
+        aesKey = (String)session.getAttribute("aesKey");
+        aesIv = (String)session.getAttribute("aesIv");
+        compId = (String)session.getAttribute("compId");
+        userNo = (String)session.getAttribute("userNo");
+        if(compId == null)  compId = (String)request.getAttribute("compId");
+
+        if(compId == null){
+            result = "{\"result\":\"failure\",\"msg\":\"" + msg.compIdNotVerified + "\"}";
+        }else if(aesKey == null || aesIv == null){
+            result = "{\"result\":\"failure\",\"msg\":\"" + msg.aesKeyNotFound + "\"}";
+        }else{
+            data = decAes(requestBody, aesKey, aesIv);
+            jarr = new JSONArray(data);
+            if(jarr != null && jarr.length() >= 12){
+                larr = new long[jarr.length()];
+                for(x = 0 ; x < jarr.length() ; x++)    larr[x] = jarr.getLong(x);
+            }
+            result = systemService.setSalesGoal(compId, userNo, year, empNo, larr);
+            if(result == null)  result = "{\"result\":\"failure\",\"msg\":\"" + msg.unknownError + "\"}";
+            else if(result.equals("permissionDenied"))  result = "{\"result\":\"failure\",\"msg\":\"" + msg.permissionDenied + "\"}";
+            else if(result.equals("ok"))  result = "{\"result\":\"ok\"}";
+            else    result = "{\"result\":\"failure\",\"msg\":\"" + msg.unknownError + "\"}";
+        }        
 
         return result;
     }
