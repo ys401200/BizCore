ad478be3c6af7bdeb38c60b1bb8a611c9fce5d0a
jo-beoungil <enky001@gmail.com>
2022-10-07 11:14:16 +0900

    코드정리

--- src/main/java/kr/co/bizcore/v1/ctrl/BusinessController.java
+++ src/main/java/kr/co/bizcore/v1/ctrl/BusinessController.java
@@ -20,407 +20,73 @@ public class BusinessController extends Ctrl{
 
     @RequestMapping(value={"/notice","/notice/{no:\\d+}"})
     public String notice(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName", pathName);
-
-        result = "/notice/list";
-
-        return result;
+        doIt(request);
+        return "/notice/list";
     } // End of /business/**
 
     @RequestMapping(value={"/schedule","/schedule/{no:\\d+}"})
     public String schedule(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/schedule/schedule";
-
-        return result;
+        doIt(request);
+        return "/schedule/schedule";
     } // End of /business/**
 
     @RequestMapping(value={"/sales","/sales/{no:\\d+}"})
     public String sales(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/sales/list";
-
-        return result;
+        doIt(request);
+        return "/sales/list";
     } // End of /business/**
 
     @RequestMapping(value={"/sopp","/sopp/{no:\\d+}"})
     public String sopp(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/sopp/list";
-
-        return result;
+        doIt(request);
+        return "/sopp/list";
     } // End of /business/**
 
     @RequestMapping("/est")
     public String est(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/est/list";
-
-        return result;
+        doIt(request);
+        return "/est/list";
     } // End of /business/**
 
     @RequestMapping(value={"/contract","/contract/{no:\\d+}"})
     public String cont(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/contract/list";
-
-        return result;
+        doIt(request);
+        return "/contract/list";
     } // End of /business/**
 
     @RequestMapping(value={"/tech","/tech/{no:\\d+}"})
     public String tech(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"));
-            uri = uri.substring(1);
-
-        tempStrArr = uri.split("/");
-
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/tech/list";
-
-        return result;
+        doIt(request);
+        return "/tech/list";
     } // End of /business/**
 
     @RequestMapping("/filebox")
     public String filebox(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/filebox/filebox";
-
-        return result;
+        doIt(request);
+        return "/filebox/filebox";
     } // End of /filebox/**
 
     @RequestMapping("/workreport")
     public String workreport(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/workreport/workreport";
-
-        return result;
+        doIt(request);
+        return "/workreport/workreport";
     } // End of /business/**
 
     @RequestMapping("/workjournal")
     public String workjournal(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"));
-            uri = uri.substring(1);
-
-        tempStrArr = uri.split("/");
-
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/workreport/workjournal";
-
-        return result;
+        doIt(request);
+        return "/workreport/workjournal";
     } // End of /business/**
 
     @RequestMapping("/goal")
     public String goal(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/goal/list";
-
-        return result;
+        doIt(request);
+        return "/goal/list";
     } // End of /business/**
 
     @RequestMapping(value={"/customer", "/customer/{no:\\d+}"})
     public String customer(HttpServletRequest request) {
-        HttpSession session = null;
-        String result = null, uri = null, pathName = null, tempStr = null;
-        String[] tempStrArr = null;
-
-        session = request.getSession();
-        SimpleUser user = (SimpleUser) session.getAttribute("user");
-        uri = request.getRequestURI();
-
-        if (uri.substring(0, 1).equals("/"))
-            uri = uri.substring(1);
-        if (uri.substring(uri.length() - 1).equals("/"))
-            uri = uri.substring(0, uri.length() - 1);
-
-        tempStrArr = uri.split("/");
-        if(tempStrArr.length == 0){
-            pathName = "root";
-        }else if(tempStrArr.length == 1){
-            pathName = tempStrArr[0];
-        }else if(tempStrArr.length > 1){
-            tempStr = tempStrArr[1];
-            pathName = tempStrArr[0];
-            pathName += tempStr.substring(0, 1).toUpperCase();
-            pathName += tempStr.substring(1).toLowerCase();
-        }
-
-        session.setAttribute("pathName",  pathName);
-
-        result = "/customer/list";
-
-        return result;
+        doIt(request);
+        return "/customer/list";
     } // End of /business/**
 }
