ff8780a7e5004ca23a2e96c6fbe89708aae7668a
jo-beoungil <enky001@gmail.com>
2022-10-07 11:36:45 +0900

    회계관리 파일 추가

---
+++ src/main/resources/static/js/accountingBankaccount/accountingBankaccount.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    init();
+    
+	setTimeout(() => {
+		$("#loadingDiv").hide();
+		$("#loadingDiv").loading("toggle");
+	}, 300);
+
+	// For Initializing Code . . . . . . .  . . . . 
+});
