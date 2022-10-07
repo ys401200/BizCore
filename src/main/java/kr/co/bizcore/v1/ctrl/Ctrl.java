98823a2b3f679d4e078db1ad54e7816c2f7a6973
jo-beoungil <enky001@gmail.com>
2022-10-07 10:59:28 +0900

    단순반복 코드 정리

--- src/main/java/kr/co/bizcore/v1/ctrl/Ctrl.java
+++ src/main/java/kr/co/bizcore/v1/ctrl/Ctrl.java
@@ -1,5 +1,8 @@
 package kr.co.bizcore.v1.ctrl;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -103,6 +106,30 @@ public abstract class Ctrl {
         if(lang == null)    return msgKor;
         if(lang.toLowerCase().equals("ko-kr"))  return msgKor;
         return msgKor;
+    }
+
+    protected void doIt(HttpServletRequest request){
+        String uri = null, pathName = null, tempStr = null;
+        String[] tempStrArr = null;
+
+        uri = request.getRequestURI();
+        if (uri.substring(0, 1).equals("/"))
+            uri = uri.substring(1);
+        if (uri.substring(uri.length() - 1).equals("/"))
+            uri = uri.substring(0, uri.length() - 1);
+
+        tempStrArr = uri.split("/");
+        if (tempStrArr.length == 0) {
+            pathName = "root";
+        } else if (tempStrArr.length == 1) {
+            pathName = tempStrArr[0];
+        } else if (tempStrArr.length > 1) {
+            tempStr = tempStrArr[1];
+            pathName = tempStrArr[0];
+            pathName += tempStr.substring(0, 1).toUpperCase();
+            pathName += tempStr.substring(1).toLowerCase();
+        }
+        request.getSession().setAttribute("pathName", pathName);
     }
     
 
