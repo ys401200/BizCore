fd9fcaa98fe0f355dfcbff8675bfc77167b2fc84
jo-beoungil <enky001@gmail.com>
2022-10-07 11:00:01 +0900

    은행계좌 도메인 클래스 추가

---
+++ src/main/java/kr/co/bizcore/v1/domain/BankAccount.java
@@ -0,0 +1,39 @@
+package kr.co.bizcore.v1.domain;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class BankAccount extends Domain{
+    protected int no;
+    protected String bankCode;
+    protected String branch;
+    protected String account;
+    protected String type;
+    protected String remark; 
+    protected String serial; // 호환성을 위해 당분간만 유지
+    protected String depositor;
+    protected Long limit;
+    protected Date established;    
+    
+}
+/* 
+cardNo
+compNo
+depName
+cardSerial
+cardDisNum
+cardDivision
+cardBank
+cardStatus
+user_card
+cardMemo
+cardIssueDate
+lastUpdTime
+regDate
+modDate
+attrib
+*/
