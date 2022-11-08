7b44582d2d94f30dcfd00567d25e94a49143610e
jo-beoungil <enky001@gmail.com>
2022-11-08 17:22:07 +0900

    매입매출 신구 클래스 작성

---
+++ src/main/java/kr/co/bizcore/v1/domain/Trade.java
@@ -0,0 +1,25 @@
+package kr.co.bizcore.v1.domain;
+
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class Trade extends Domain{
+
+    protected int no;
+    protected Date dt;
+    protected String belongTo;
+    protected int writer;
+    protected String type;
+    protected int product;
+    protected int customer;
+    protected String taxbill;
+    protected String title;
+    protected int qty;
+    protected long price;
+    protected long vat;
+    protected String remark;
+    protected String related;    
+    
+}
