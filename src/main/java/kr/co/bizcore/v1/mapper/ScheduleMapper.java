b2e6172912ec5173385cdd3f88212566ac06e76f
jo-beoungil <enky001@gmail.com>
2022-11-09 16:56:07 +0900

    쿼리 오류 수정

--- src/main/java/kr/co/bizcore/v1/mapper/ScheduleMapper.java
+++ src/main/java/kr/co/bizcore/v1/mapper/ScheduleMapper.java
@@ -110,9 +110,9 @@ public interface ScheduleMapper {
 
     @Select("SELECT 'schedule' AS job, schedno AS no, schedFrom AS `from`, schedtitle AS title, schedDesc AS content, schedCheck AS report FROM swc_sched WHERE  attrib NOT LIKE 'XXX%' AND userno = #{writer} AND schedfrom < #{end} AND schedto >= #{start} AND compno = (SELECT compno FROM swc_company WHERE compid = #{compId}) " +
                 "UNION ALL " + 
-                "SELECT 'sales' AS job, salesno AS no, salesfrdatetime AS `from`, salestitle AS title, salesdesc AS content, salesCheck AS report FROM swc_sales WHERE attrib = 11111 AND userno = #{writer} AND salesfrdatetime < #{end} AND salestodatetime >= #{start} AND compno = (SELECT compno FROM swc_company WHERE compid = #{compId}) " +
+                "SELECT 'sales' AS job, salesno AS no, salesfrdatetime AS `from`, salestitle AS title, salesdesc AS content, salesCheck AS report FROM swc_sales WHERE attrib NOT LIKE 'XXX%' AND userno = #{writer} AND salesfrdatetime < #{end} AND salestodatetime >= #{start} AND compno = (SELECT compno FROM swc_company WHERE compid = #{compId}) " +
                 "UNION ALL " + 
-                "SELECT 'tech' AS job, techdno AS no, techdfrom AS `from`, techdtitle AS title, techddesc AS content, techdCheck AS report FROM swc_techd WHERE attrib = 11111 AND userno = #{writer} AND techdfrom < #{end} AND techdto >= #{start} AND compno = (SELECT compno FROM swc_company WHERE compid = #{compId})")
+                "SELECT 'tech' AS job, techdno AS no, techdfrom AS `from`, techdtitle AS title, techddesc AS content, techdCheck AS report FROM swc_techd WHERE attrib NOT LIKE 'XXX%' AND userno = #{writer} AND techdfrom < #{end} AND techdto >= #{start} AND compno = (SELECT compno FROM swc_company WHERE compid = #{compId})")
     public List<Schedule> getScheduleListForReport(@Param("compId") String compId, @Param("start") Date start, @Param("end") Date end, @Param("writer") int writer);
 
     @Update("UPDATE swc_sreport SET attrib = 'XXXXX' WHERE userno = #{userNo} AND weeknum = #{week} AND compno = (SELECT compno FROM swc_company WHERE compid = #{compId})")
