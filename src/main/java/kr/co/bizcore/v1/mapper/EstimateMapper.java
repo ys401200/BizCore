797962e8736acd0759a67d5182496727c48f2c1f
jo-beoungil <enky001@gmail.com>
2022-11-10 11:49:14 +0900

    견적 항목 테이블 삭제 부분 반영

--- src/main/java/kr/co/bizcore/v1/mapper/EstimateMapper.java
+++ src/main/java/kr/co/bizcore/v1/mapper/EstimateMapper.java
@@ -17,11 +17,11 @@ public interface EstimateMapper {
     public List<HashMap<String, String>> getItems(@Param("compId") String compId);
 
     // 견적 최종버전 기준 목록을가져오는 메서드
-    @Select("SELECT a.estmno AS no, formName AS form, title, CAST(version AS CHAR) AS version, CAST(UNIX_TIMESTAMP(dt) * 1000 AS CHAR) AS dt, cast((SELECT b.total FROM (SELECT estmno, version, SUM(price * qty) AS total FROM bizcore.estimate_items WHERE deleted IS NULL AND compId = #{compId} GROUP BY estmno, version) b WHERE a.estmno = b.estmno AND a.version = b.version) AS CHAR) AS total FROM bizcore.estimate a, (SELECT estmno, max(version) v FROM bizcore.estimate WHERE deleted IS NULL AND compid = #{compId} GROUP BY estmno) c WHERE deleted IS NULL AND compId = #{compId} AND a.estmno = c.estmno AND a.version = c.v ORDER BY created DESC")
+    @Select("SELECT a.estmno AS no, formName AS form, title, CAST(version AS CHAR) AS version, CAST(UNIX_TIMESTAMP(dt) * 1000 AS CHAR) AS dt FROM bizcore.estimate a, (SELECT estmno, max(version) v FROM bizcore.estimate WHERE deleted IS NULL AND compid = #{compId} GROUP BY estmno) c WHERE deleted IS NULL AND compId = #{compId} AND a.estmno = c.estmno AND a.version = c.v ORDER BY created DESC")
     public List<HashMap<String, String>> getEstmList(@Param("compId") String compId);
 
     // 견적 버전 목록을 가져오는 메서드
-    @Select("SELECT estmNo AS no, formName AS form, title, CAST(version AS CHAR) AS version, CAST(UNIX_TIMESTAMP(dt) * 1000 AS CHAR) AS dt, `exp`, CAST(writer AS CHAR) AS writer, doc, CAST(width AS CHAR) AS width, CAST(height AS CHAR) AS height, remarks, related, CAST(p AS CHAR) AS total FROM bizcore.estimate a, (SELECT version v, SUM(qty*price) p FROM bizcore.estimate_items WHERE deleted IS NULL AND compId = #{compId} AND estmNo  = #{no} GROUP BY version) b WHERE deleted IS NULL AND a.version = b.v AND compId = #{compId} AND estmNo = #{no} ORDER BY version")
+    @Select("SELECT estmNo AS no, formName AS form, title, version, CAST(UNIX_TIMESTAMP(dt) * 1000 AS CHAR) AS dt, `exp`, CAST(writer AS CHAR) AS writer, doc, CAST(width AS CHAR) AS width, CAST(height AS CHAR) AS height, remarks, related FROM bizcore.estimate a WHERE deleted IS NULL AND compId = #{compId} AND estmNo = #{NO} ORDER BY version")
     public List<HashMap<String, String>> getEstmVersionList(@Param("compId") String compId, @Param("no") String estmNo);
 
     // 견적의 기본정보를 가여오는 메서드
