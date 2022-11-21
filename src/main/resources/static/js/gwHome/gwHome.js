b576307c9e7234b70d6f98e4ba92c3259bfaf157
5079daeng <myy32612@gmail.com>
2022-11-21 16:38:05 +0900

    모달 z index 추가함

--- src/main/resources/static/js/gwHome/gwHome.js
+++ src/main/resources/static/js/gwHome/gwHome.js
@@ -14,19 +14,19 @@ $(document).ready(() => {
 
     if ($(".card").css("width").split("p")[0] < 762) {
       console.log("확인1");
-      $(".card").css("grid-template-columns", "repeat(3, 1fr)");
+      $(".card").css("grid-template-columns", "repeat(4, 1fr)");
     } else if ($(".card").css("width").split("p")[0] < 1016) {
       console.log("확인2");
-      $(".card").css("grid-template-columns", "repeat(4, 1fr)");
+      $(".card").css("grid-template-columns", "repeat(5, 1fr)");
     } else if ($(".card").css("width").split("p")[0] < 1270) {
       console.log("확인3");
-      $(".card").css("grid-template-columns", "repeat(5, 1fr)");
+      $(".card").css("grid-template-columns", "repeat(6, 1fr)");
     } else if ($(".card").css("width").split("p")[0] > 1270) {
       console.log("확인4");
-      $(".card").css("grid-template-columns", "repeat(6, 1fr)");
+      $(".card").css("grid-template-columns", "repeat(7, 1fr)");
     } else if ($(".card").css("width").split("p")[0] > 1440) {
       console.log("확인4");
-      $(".card").css("grid-template-columns", "repeat(7, 1fr)");
+      $(".card").css("grid-template-columns", "repeat(8, 1fr)");
     }
 
     // if ($(".waitDiv").css("width").split("p")[0] < 762) {
