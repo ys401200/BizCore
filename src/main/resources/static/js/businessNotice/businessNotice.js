e720f789c9c4b3651809126c91246160b060d007
5079daeng <myy32612@gmail.com>
2022-11-29 15:07:31 +0900

    게시판 레이아웃 일부 수정

--- src/main/resources/static/js/businessNotice/businessNotice.js
+++ src/main/resources/static/js/businessNotice/businessNotice.js
@@ -11,9 +11,9 @@ $(document).ready(() => {
 
 // API 서버에서 공지사항 리스트를 가져오는 함수
 function getNoticeList() {
-	let url, method, data, type; 
+	let url, method, data, type;
 	url = "/api/notice";
-	method ="get";
+	method = "get";
 	data = "";
 	type = "list";
 	crud.defaultAjax(url, method, data, type, noticeSuccessList, noticeErrorList);
@@ -26,9 +26,9 @@ function drawNoticeList() {
 		msg.set("등록된 공지사항이 없습니다");
 	}
 	else {
-		if(storage.searchDatas === undefined){
+		if (storage.searchDatas === undefined) {
 			jsonData = storage.noticeList;
-		}else{
+		} else {
 			jsonData = storage.searchDatas;
 		}
 	}
@@ -52,21 +52,21 @@ function drawNoticeList() {
 		},
 	];
 
-	if(jsonData === ""){
+	if (jsonData === "") {
 		str = [
 			{
 				"setData": undefined,
 				"col": 4,
 			},
 		];
-		
+
 		data.push(str);
-	}else{
+	} else {
 		for (let i = (result[0] - 1) * result[1]; i < result[2]; i++) {
 			disDate = dateDis(jsonData[i].created, jsonData[i].modified);
 			setDate = dateFnc(disDate, "mm-dd");
 			let userName = storage.user[jsonData[i].writer].userName;
-	
+
 			str = [
 				{
 					"setData": setDate,
@@ -81,12 +81,12 @@ function drawNoticeList() {
 					"align": "center",
 				},
 			]
-	
+
 			fnc = "noticeDetailView(this)";
 			ids.push(jsonData[i].no);
 			data.push(str);
 		}
-	
+
 		let pageNation = createPaging(pageContainer[0], result[3], "pageMove", "drawNoticeList", result[0]);
 		pageContainer[0].innerHTML = pageNation;
 	}
@@ -95,7 +95,7 @@ function drawNoticeList() {
 
 	let path = $(location).attr("pathname").split("/");
 
-	if(path[3] !== undefined && jsonData !== null){
+	if (path[3] !== undefined && jsonData !== null) {
 		let content = $(".gridContent[data-id=\"" + path[3] + "\"]");
 		noticeDetailView(content);
 	}
@@ -114,25 +114,25 @@ function noticeDetailView(e) {// 선택한 그리드의 글 번호 받아오기
 	crud.defaultAjax(url, method, data, type, noticeSuccessView, noticeErrorView);
 } // End of noticeDetailView()
 
-function noticeSuccessList(result){
+function noticeSuccessList(result) {
 	storage.noticeList = result;
 
-	if(storage.customer === undefined || storage.code === undefined || storage.dept === undefined){
+	if (storage.customer === undefined || storage.code === undefined || storage.dept === undefined) {
 		window.setTimeout(drawNoticeList, 600);
 		window.setTimeout(addSearchList, 600);
 		window.setTimeout(searchContainerSet, 600);
-	}else{
+	} else {
 		window.setTimeout(drawNoticeList, 200);
 		window.setTimeout(addSearchList, 200);
 		window.setTimeout(searchContainerSet, 200);
 	}
 }
 
-function noticeErrorList(){
+function noticeErrorList() {
 	msg.set("에러");
 }
 
-function noticeSuccessView(result){
+function noticeSuccessView(result) {
 	let html = "", btnHtml = "", datas, title, content, writer, dataArray, disDate, setDate, notIdArray;
 	detailSetFormList(result);
 	title = (result.title === null || result.title === "" || result.title === undefined) ? "" : result.title;
@@ -177,26 +177,26 @@ function noticeSuccessView(result){
 	storage.gridContent.after(html);
 	notIdArray = ["writer", "created"];
 	detailTrueDatas(datas);
-	
-	if(storage.my == storage.formList.writer){
+
+	if (storage.my == storage.formList.writer) {
 		btnHtml += "<button type=\"button\" class=\"updateBtn\" onclick=\"enableDisabled(this, 'noticeUpdate();', '" + notIdArray + "');\">수정</button>";
 		btnHtml += "<button type=\"button\" onclick=\"noticeDelete();\">삭제</button>";
 	}
 
 	btnHtml += "<button type='button' onclick='detailBoardContainerHide();'><i class=\"fa-solid fa-xmark\"></i></button>";
 	$(".detailBtns").html(btnHtml);
-	
+
 	setTimeout(() => {
 		ckeditor.config.readOnly = true;
 		window.setTimeout(setEditor, 100);
 	}, 100);
 }
 
-function noticeErrorView(){
+function noticeErrorView() {
 	msg.set("에러");
 }
 
-function noticeInsertForm(){
+function noticeInsertForm() {
 	let html, dataArray;
 
 	dataArray = [
@@ -244,18 +244,18 @@ function noticeInsertForm(){
 		ckeditor.config.readOnly = false;
 		window.setTimeout(setEditor, 100);
 	}, 100);
-	
+
 	setTimeout(() => {
 		document.getElementsByClassName("cke_textarea_inline")[0].style.height = "300px";
 	}, 300);
 }
 
-function noticeInsert(){
-	if(title === ""){
+function noticeInsert() {
+	if (title === "") {
 		msg.set("제목을 입력해주세요.");
 		$("#title").focus();
 		return false;
-	}else{
+	} else {
 		let url, method, data, type;
 		formDataSet();
 		url = "/api/notice";
@@ -269,21 +269,21 @@ function noticeInsert(){
 
 }
 
-function noticeSuccessInsert(){
+function noticeSuccessInsert() {
 	location.reload();
 	msg.set("등록완료");
 }
 
-function noticeErrorInsert(){
+function noticeErrorInsert() {
 	msg.set("등록에러");
 }
 
-function noticeUpdate(){
-	if(title === ""){
+function noticeUpdate() {
+	if (title === "") {
 		msg.set("제목을 입력해주세요.");
 		$("#title").focus();
 		return false;
-	}else{
+	} else {
 		let url, method, data, type;
 		formDataSet();
 		url = "/api/notice/" + storage.formList.no;
@@ -296,57 +296,57 @@ function noticeUpdate(){
 	}
 }
 
-function noticeSuccessUpdate(){
+function noticeSuccessUpdate() {
 	location.reload();
 	msg.set("수정완료");
 }
 
-function noticeErrorUpdate(){
+function noticeErrorUpdate() {
 	msg.set("수정에러");
 }
 
-function noticeDelete(){
+function noticeDelete() {
 	let url, method, data, type;
 
-	if(confirm("정말로 삭제하시겠습니까??")){
+	if (confirm("정말로 삭제하시겠습니까??")) {
 		url = "/api/notice/" + storage.formList.no;
 		method = "delete";
 		data = "";
 		type = "delete";
-	
+
 		crud.defaultAjax(url, method, data, type, noticeSuccessDelete, noticeErrorDelete);
-	}else{
+	} else {
 		return false;
 	}
 }
 
-function noticeSuccessDelete(){
+function noticeSuccessDelete() {
 	location.reload();
 	msg.set("삭제완료");
 }
 
-function noticeErrorDelete(){
+function noticeErrorDelete() {
 	msg.set("삭제에러");
 }
 
-function searchInputKeyup(){
+function searchInputKeyup() {
 	let searchAllInput, tempArray;
 	searchAllInput = $("#searchAllInput").val();
 	tempArray = searchDataFilter(storage.noticeList, searchAllInput, "input");
 
-	if(tempArray.length > 0){
+	if (tempArray.length > 0) {
 		storage.searchDatas = tempArray;
-	}else{
+	} else {
 		storage.searchDatas = "";
 	}
 
 	drawNoticeList();
 }
 
-function addSearchList(){
+function addSearchList() {
 	storage.searchList = [];
 
-	for(let i = 0; i < storage.noticeList.length; i++){
+	for (let i = 0; i < storage.noticeList.length; i++) {
 		let no, title, writer, disDate, setDate;
 		no = storage.noticeList[i].no;
 		title = storage.noticeList[i].title;
@@ -357,20 +357,20 @@ function addSearchList(){
 	}
 }
 
-function searchSubmit(){
+function searchSubmit() {
 	let dataArray = [], resultArray, eachIndex = 0, searchTitle, searchCreatedFrom;
 
 	searchTitle = $("#searchTitle").val();
 	searchWriter = $("#searchWriter").val();
 	searchCreatedFrom = ($("#searchCreatedFrom").val() === "") ? "" : $("#searchCreatedFrom").val().replaceAll("-", "") + "#created" + $("#searchCreatedTo").val().replaceAll("-", "");
-	
+
 	let searchValues = [searchTitle, searchWriter, searchCreatedFrom];
 
-	for(let i = 0; i < searchValues.length; i++){
-		if(searchValues[i] !== "" && searchValues[i] !== undefined && searchValues[i] !== null){
+	for (let i = 0; i < searchValues.length; i++) {
+		if (searchValues[i] !== "" && searchValues[i] !== undefined && searchValues[i] !== null) {
 			let tempArray = searchDataFilter(storage.noticeList, searchValues[i], "multi");
-			
-			for(let t = 0; t < tempArray.length; t++){
+
+			for (let t = 0; t < tempArray.length; t++) {
 				dataArray.push(tempArray[t]);
 			}
 
@@ -379,13 +379,13 @@ function searchSubmit(){
 	}
 
 	resultArray = searchMultiFilter(eachIndex, dataArray, storage.noticeList);
-	
+
 	storage.searchDatas = resultArray;
 
-	if(storage.searchDatas.length == 0){
+	if (storage.searchDatas.length == 0) {
 		msg.set("찾는 데이터가 없습니다.");
 		storage.searchDatas = storage.noticeList;
 	}
-	
+
 	drawNoticeList();
 }
--- src/main/resources/static/js/mypage/mypage.js
+++ src/main/resources/static/js/mypage/mypage.js
@@ -26,7 +26,7 @@ function showCheckPwForm() {
 
 function checkPw() {
 	let insertedPw = $("#pwInput").val(); // 입력받은 비밀번호 
-	insertedPw = cipher.encAes(insertedPw).replaceAll("/","*");
+	insertedPw = cipher.encAes(insertedPw).replaceAll("/", "*");
 	let url;
 
 	url = apiServer + "/api/my/" + insertedPw;
@@ -81,7 +81,7 @@ function drawMyPage(data) {
 		"<div><div>이메일</div><input type='text' class='change email' value='" + data.email + "'disabled /></div>" +
 		"<div><div>핸드폰번호</div><input type='text' class='change cellPhone' value='" + data.cellPhone + "'disabled/></div>" +
 		"<div><div>전화번호</div><input type='text' class='change homePhone' value='" + data.homePhone + "'disabled/></div>" +
-		"<div class='threeGrid'><div>우편번호</div><input type='text' id='postCode' class='zipCode' value='" + data.zipCode + "'disabled/><button type='button' class='daumBtn' onclick='daumPostCode('postCode', 'mainAddress', 'detailAddress')'>검색</button></div>" +
+		"<div class='threeGrid'><div>우편번호</div><input type='text' id='postCode' class='zipCode' value='" + data.zipCode + "'disabled/><button type='button' class='daumBtn' onclick='doDaumnPostCode()'>검색</button></div>" +
 		"<div><div>주소</div><input type='text' id='mainAddress' class='address' value='" + maindAddress + "'disabled/></div>" +
 		"<div><div>상세 주소</div><input type='text' id='detailAddress' class='change address' value='" + detailAddress + "'disabled/></div>" +
 		"<div><div>생년월일</div><input type='text' value='" + data.birthDay + "'disabled/></div>" +
@@ -98,7 +98,11 @@ function drawMyPage(data) {
 	$(".forPhoto").html(rightHtml);
 	$(".daumBtn").hide();
 	$(".modPhoto").hide();
+  
+}
 
+function doDaumnPostCode() {
+daumPostCode('postCode', 'mainAddress', 'detailAddress');
 }
 
 
